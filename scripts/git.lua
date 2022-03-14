cc530ca11c5051b03af929f0315ac4aec3b69ec7
CoyTorreblanca <coydiego@knights.ucf.edu>
2022-03-13 20:14:42 -0400

    Add script to download github.

---
+++ scripts/git.lua
@@ -0,0 +1,37 @@
+BASE_URL = "https://raw.githubusercontent.com/Coy-Torreblanca/ComputerCraft/master"
+
+-- add files here as our repo grows
+FILES = {
+	"/scripts/git.lua",
+}
+
+function getFile(filename)
+	-- download file from github
+	local resp = http.get(BASE_URL .. filename)
+	if resp == nil then
+		print("Download failed for " .. BASE_URL .. filename)
+		return
+	end
+
+	-- write file
+	local file = fs.open(filename, "w")
+	file.write(resp.readAll())
+	file.close()
+end
+
+-- entry point
+-- make sure directories exist
+if not fs.exists("/lib") then
+	fs.makeDir("/lib")
+end
+if not fs.exists("/bin") then
+	fs.makeDir("/bin")
+end
+if not fs.exists("/scripts") then
+	fs.makeDir("/scripts")
+end
+
+for _, file in ipairs(FILES) do
+	print("Downloading " .. file .. "...")
+	getFile(file)
+end
