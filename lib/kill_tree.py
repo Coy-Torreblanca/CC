9daa948f6d3f0c7b205b3f9fa80696549682e0e7
CoyTorreblanca <coydiego@knights.ucf.edu>
2022-03-15 00:23:51 -0400

    add kill_tree

--- lib/kill_tree.py
+++ lib/kill_tree.py
@@ -5,6 +5,35 @@ from cc import turtle, import_file
 nav = import_file("/lib/nav.py")
 nav = nav.nav()
 
+tree_db = ["minecraft:oak_log"]
+
 
 def kill_tree():
-    print(nav.direction)
+    for _ in range(4):
+        item = turtle.inspect()
+
+        if item and item["name"] in tree_db:
+            turtle.dig()
+            turtle.forward()
+            direction = nav.direction
+            kill_tree()
+            nav.turn_to(direction)
+            nav.back()
+
+    item = turtle.inspectUp()
+    if item and item["name"] in tree_db:
+        turtle.digUp()
+        turtle.up()
+        direction = nav.direction
+        kill_tree()
+        nav.turn_to(direction)
+        nav.down()
+
+    item = turtle.inspectUp()
+    if item and item["name"] in tree_db:
+        turtle.digDown()
+        turtle.down()
+        direction = nav.direction
+        kill_tree()
+        nav.turn_to(direction)
+        nav.up()
