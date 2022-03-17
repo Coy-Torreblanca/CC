223e5ddbbe0897e9f7c7b94dd568e6eae0d2016e
CoyTorreblanca <coydiego@knights.ucf.edu>
2022-03-16 21:14:01 -0400

    Add inventory class in inventory.

--- lib/inventory.py
+++ lib/inventory.py
@@ -32,3 +32,47 @@ def is_full():
         if not turtle.getItemDetail():
             return False
     return True
+
+
+class inventory:
+    items = {}
+    current_slot = 0
+    position = None
+
+    def __init__(self, position):
+        self.position = position
+
+    def is_full(self):
+        if self.current_slot > 16:
+            for _, count in self.items.values():
+                if count < 64:
+                    return False
+            return True
+        return False
+
+    def is_full_item(self, name):
+        if self.current_slot > 16:
+            if self.items[name][1] == 64:
+                return True
+        return False
+
+    def add_item(self, name, count):
+        if self.is_full_item(name):
+            return False
+
+        while count:
+            if name in self.items:
+                slot_quantity = self.items[name][1]
+                to_drop = 64 - slot_quantity
+                to_drop = to_drop if to_drop <= count else count
+                self.items[name][1] += to_drop
+                if self.items[name][1] == 64:
+                    self.items[name][0] = self.current_slot
+                    self.current_slot += 1
+                count -= to_drop
+                if self.is_full_item(name):
+                    return count == 0
+            else:
+                self.items[name] = [self.current_slot, 0]
+                self.current_slot += 1
+        return True
