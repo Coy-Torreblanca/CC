ce0ed0c53c7991e4bcce8d175c249bacd7bb895e
CoyTorreblanca <coydiego@knights.ucf.edu>
2022-03-16 21:17:34 -0400

    fixup

--- lib/inventory.py
+++ lib/inventory.py
@@ -45,7 +45,7 @@ class inventory:
         self.max_storage = max_storage if max_storage else self.max_storage
 
     def is_full(self):
-        if self.current_slot > self.max_storage:
+        if self.current_slot < self.max_storage:
             for _, count in self.items.values():
                 if count < 64:
                     return False
