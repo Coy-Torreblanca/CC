4b20133970833d4b0a09619f7c295062da9506e0
CoyTorreblanca <coydiego@knights.ucf.edu>
2022-03-20 16:31:31 -0400

    fixup

--- bin/chest_quarry.py
+++ bin/chest_quarry.py
@@ -20,7 +20,7 @@ class chest_quarry:
         self.db = data.dig_map()
         self.job = "chest_quarry_" + str(self.nav.locate())
         self.chest_management = chest_management.chest_management(
-            self.job, self.nav, self.inventory, self.turtle
+            self.job, self.nav, self.inventory, turtle
         )
 
     def dig(self, turtle_direction):
